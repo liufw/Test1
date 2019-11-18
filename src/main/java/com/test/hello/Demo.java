f95ee9a0f51d312a00d51d1ce1625f71d33d6540
liufw <liufw6868@163.com>
2019-11-18 19:07:05 +0800

    Change

--- src/main/java/com/test/hello/Demo.java
+++ src/main/java/com/test/hello/Demo.java
@@ -3,11 +3,7 @@ package com.test.hello;
 public class Demo {
     public static void main(String[] args)throws Exception {
 
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.println("1111111");
+
     }
 }
