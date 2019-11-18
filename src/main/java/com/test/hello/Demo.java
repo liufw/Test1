1b94f6c78bd1a052caa9d7e7feb0f5681f236c3a
liufw <liufw6868@163.com>
2019-11-18 16:11:11 +0800

    Hello Wrold!

---
+++ src/main/java/com/test/hello/Demo.java
@@ -0,0 +1,9 @@
+package com.test.hello;
+
+public class Demo {
+    public static void main(String[] args)throws Exception {
+
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
